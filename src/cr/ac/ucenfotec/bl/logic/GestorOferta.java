73f8ddd1d368248f95ac9923f8885caddeea82cb
jose-ucenfotec <jbarrantessa@ucenfotec.ac.cr>
2026-04-23 22:59:53 -0600

    Update GestorOferta.java

--- src/cr/ac/ucenfotec/bl/logic/GestorOferta.java
+++ src/cr/ac/ucenfotec/bl/logic/GestorOferta.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 
 public class GestorOferta {
 
-    // Las validaciones (solo coleccionistas, no ofertar en propia subasta)
-    // las manejan los triggers de la BD.
+
     public static String registrarOferta(int idSubasta, String oferenteId, double precioOfertado)
             throws SQLException, IOException, ClassNotFoundException {
         return DAOOferta.insertarOferta(idSubasta, oferenteId, precioOfertado);
